ac442ac9c8df549478421cec4e43a1cd05848af6
ivan <royal_88@mail.ru>
2022-10-06 20:43:25 +0300

    some bullshit

--- src/esvo_in_data_init.c
+++ src/esvo_in_data_init.c
@@ -3,4 +3,14 @@
 
 void esvo_in_data_init(esvo_in_data_t* ptr) {
     ptr->counter = 0;
+    ptr->mfci_in_b.counter = 0;
+    ptr->mfci_in_b.mfci_in_25hz_b.counter = 0;
+    ptr->mfci_in_b.mfci_in_12hz_b.counter = 0;
+    ptr->mfci_in_b.mfci_in_6hz_b.counter = 0;
+    ptr->mfci_in_b.mfci_in_1hz_b.counter = 0;
+    ptr->mfci_in_b.mfci_in_25hz_b.bis_b.counter = 0;
+    ptr->mfci_in_b.mfci_in_25hz_b.bis_b.sa_1_b.counter = 0;
+    ptr->mfci_in_b.mfci_in_25hz_b.bis_b.sa_1_b.signals_word_1_b.dv1_turb_overheating = 1;
 }
+
+/* ptr->mfci_in_b.blocks_crc_b[CRC_SA_COUNT] = 0; */
